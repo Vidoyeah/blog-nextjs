1d60562c0dc82012030a659dfabb71fe8a3dd1f1
Vidoyeah <vidojer@gmail.com>
2023-08-28 13:36:51 +0200

    render markdown as JSX

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,3 +1,4 @@
+import ReactMarkdown from 'react-markdown';
 import PostHeader from './post-header';
 
 import classes from './post-content.module.css';
@@ -19,7 +20,7 @@ function PostContent() {
         title={DUMMY_POST.title}
         image={imagePath}
       />
-      {DUMMY_POST.content}
+      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
     </article>
   );
 }
