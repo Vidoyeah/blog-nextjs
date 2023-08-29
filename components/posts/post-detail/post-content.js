056dc1eb8770702de48af0c429986b81dc8bf2e5
Vidoyeah <vidojer@gmail.com>
2023-08-29 12:14:29 +0200

    render images witx next/image (from markdown)

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -2,11 +2,43 @@ import ReactMarkdown from 'react-markdown';
 import PostHeader from './post-header';
 
 import classes from './post-content.module.css';
+import Image from 'next/image';
 
 function PostContent(props) {
   const { post } = props;
 
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.properties.src}`}
+    //       alt={image.properties.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.properties.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    },
+  };
 
   return (
     <article className={classes.content}>
@@ -14,7 +46,7 @@ function PostContent(props) {
         title={post.title}
         image={imagePath}
       />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
